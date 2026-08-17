c4bbc48cc9241832f2db8c258589882abd6511ee
Nurislam <nurislambs14l@gmail.com>
2026-08-17 09:42:11 +0600

    роект

--- apps/nurai/models.py
+++ apps/nurai/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField("Имя", max_length=100)
+    age = models.IntegerField("Возраст")
+    city = models.CharField("Город", max_length=100)
+    email = models.EmailField("Email")
+    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Студент"
+        verbose_name_plural = "Студенты"
