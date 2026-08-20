2720cf2b9434b1351137439e41a1749cfc3f287e
Nurislam <nurislambs14l@gmail.com>
2026-08-20 13:56:30 +0600

    Update project

--- apps/nurai/views.py
+++ apps/nurai/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
+
+def index(request):
+    students = Student.objects.all()
+    return render(request, 'index.html', locals())
